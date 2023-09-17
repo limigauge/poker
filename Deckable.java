773924569593385eb77745cb1bb02b5eb673d435
limigauge <115133199+limigauge@users.noreply.github.com>
2023-09-16 18:59:01 -0700

    Update Deckable.java

--- Deckable.java
+++ Deckable.java
@@ -6,11 +6,11 @@ public interface Deckable
 	public static final int NUM_CARDS = 52;
 	
 	//Methods:
-	public void shuffle();  //This must shuffle the deck randomly.
+	public void shuffle();  
 	
-	public void returnToDeck(LinkedList<Cardable> discarded);  //This must return the cards (Cardables) that were drawn previously (passed as a LinkedList<Cardable> parameter) back to the deck (do not recreate new cards, the same cards that were drawn must go back).
+	public void returnToDeck(LinkedList<Cardable> discarded);  
 	
-	public Cardable drawACard(boolean faceUp);  //This deals the card (Cardable) that is at the top of the deck, either with the face up (true) or down (false).
+	public Cardable drawACard(boolean faceUp); 
 	
 	
 }
