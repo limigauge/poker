5a7db86a1ac589f720f0c6b5c2d1e825823a4064
limigauge <115133199+limigauge@users.noreply.github.com>
2023-09-16 19:02:01 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,4 @@
 /*
- *   CLASS : Player.java
- *   Author: Michelle Li, 7866927
- *
  *   REMARKS: Class for Players. Hold a player's fields such as their hand and their number of wins so far. Is able to increment number of wins.
  */
 
