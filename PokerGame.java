cc3acb218452c39385c1eaad83d70726de05624f
limigauge <115133199+limigauge@users.noreply.github.com>
2023-10-03 18:57:15 -0700

    Update PokerGame.java

--- PokerGame.java
+++ PokerGame.java
@@ -1,9 +1,5 @@
 //-----------------------------------------
 // REMARKS: This is the class containing the main method that will run the game. 
-// You should add the appropriate constructor call in the main, to build a GameLogicable.
-// You should not add any more code here than that constructor call.
-//
-//
 //-----------------------------------------
 
 public class PokerGame
